e1845fc37af12bdc218db6254a376e21ec5f951f
Ale <alejandrohierro@gmail.com>
2023-01-16 17:47:12 -0300

    logs y compression

--- src/routes/info.ts
+++ src/routes/info.ts
@@ -3,6 +3,7 @@ import Config from "../config/config";
 
 const router = express.Router();
 
+// with gzip 543 bytes, without gzip 567 bytes
 router.get('', (req, res) => {
     res.json({
         argv: Config.argv,
